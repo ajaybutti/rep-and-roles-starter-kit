5d6fe36e854a3d7b44151a98657d43905e084581
Nithin <nithinkatkam504106@gmail.com>
2024-07-01 01:47:25 +0530

    op-bounty

--- packages/foundry/script/DeployDemo.s.sol
+++ packages/foundry/script/DeployDemo.s.sol
@@ -15,7 +15,7 @@ import {ReputationFaucet} from "../contracts/Reputation/ReputationFaucet.sol";
 contract DeployDemoScript is ScaffoldETHDeploy {
     error InvalidPrivateKey(string);
 
-    address controller = 0x2F15D4A66D22ecC6967928b6A76Ab06897b05676; //replace with burner or other address from wallet!
+    address controller = 0x94982754E9d9500B7cafFCBF3361Fb806ACC722B; //replace with burner or other address from wallet!
 
     function run() external {
         uint256 deployerPrivateKey = setupLocalhostEnv();
